44e92a483f4cb8191ee8ed992be6a858d4ad7ca9
Wouter de Bruin <w.de.bruin@live.nl>
2011-09-26 13:37:23 +0200

    front end

---
+++ frontend/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-GB">
+<head>
+	<title>Het boek was beter</title>
+	<meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8" />
+	<meta name="description" content="The Perfect 2 Column Liquid Layout (double page): No CSS hacks. SEO friendly. iPhone compatible." />
+	<meta name="keywords" content="The Perfect 2 Column Liquid Layout (double page): No CSS hacks. SEO friendly. iPhone compatible." />
+	<meta name="robots" content="index, follow" />
+	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+	<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+
+<div id="header">
+	<h1>The Perfect boekwasbeter template</h1>
+	<h2>Only for millionaires.</h2>
+</div>
+<div class="colmask doublepage">
+	<div class="colleft">
+		<div class="col1">
+			<!-- Column 1 start -->
+			<h2>Recommended books</h2>
+			
+			<textarea class="leftta">Book 1 blablabla</textarea>
+			<!-- Column 1 end -->
+		</div>
+		
+		<div class="col2">
+			<!-- Column 2 start -->
+			<h2>Movies I like</h2>
+			Enter movie:
+			<form>
+			  <input type="text"></input>
+			  <input type="submit" value="Enter!">
+			</form>
+			
+			<textarea class="rightta">Harry Potter goblet of fire</textarea>
+			<!-- Column 2 end -->
+
+		</div>
+	</div>
+</div>
+<div id="footer">
+
+</div>
+
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+_uacct = "UA-1848067-8";
+urchinTracker();
+</script>
+
+</body>
+</html>
